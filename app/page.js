477a415ff5481595ea334d5e3baa2334cd3e0b1b
Yenuka Imandiv <disnakotagama@gmail.com>
2024-05-06 22:20:51 +0530

    ffrfrfrfrf
    yenuka

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Navbar from "./components/Navbar";
+import OurProducts from "./components/ourproducts";
+
+export default function Home() {
+  return (
+  <div>
+<>    
+    <Navbar />
+    <OurProducts />
+</>
+  </div>
+  );
+}
