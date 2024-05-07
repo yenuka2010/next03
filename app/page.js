84d96d9102f33e5360ac0f714eabdd09ee7e0767
Yenuka Imandiv <disnakotagama@gmail.com>
2024-05-07 10:53:27 +0530

    tt
    rtrt

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
-import OurProducts from "./components/ourproducts";
+import OurProducts from "./components/ourproduct";
+import Footer from "./components/footer";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
 <>    
     <Navbar />
     <OurProducts />
+    <Footer />
 </>
   </div>
   );
